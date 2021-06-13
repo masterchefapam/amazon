291d3e63ec65e23fa763ba6929bb1d5c788e0716
Nes Abadiano <ishe@MasterChefApam.local>
2021-06-13 13:29:25 +0800

    5th changes

--- components/Product.js
+++ components/Product.js
@@ -12,9 +12,7 @@ function Product({id,title,price,description,category,image}) {
     const [hasPrime] = useState(Math.random() < 0.5)
 
     return (
-        <div className="relative flex flex-col m-5 bg-white cursor:pointer
-                    z-30 p-10 rounded-md shadow-md 
-                      hover:scale-75 transform transition delay-150 duration-500 ease-in-out">
+        <div className="relative flex flex-col m-5 bg-white z-30 p-10 rounded-md">
            <p className="absolute top-2  right-2 italic text-xs text-gray-400">{category}</p> 
            <Image src={image} 
                 height={200} width={200}
